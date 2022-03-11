497e4f68b906ce1b935be87940b16c97395c9f09
Leandro-SI <llmanutencao@bol.com.br>
2022-03-11 10:03:36 -0300

    config rotas para User

---
+++ src/routes/userRoute.js
@@ -0,0 +1,52 @@
+const Joi = require('joi')
+const UsersController = require('../controllers/users')
+const UserModel = require('../models/users')
+
+const usersController = new UsersController(UserModel)
+
+const userRoute = (server) => {
+
+    server.route({
+        method: 'GET',
+        path: '/users/{id?}',
+        handler: (request, h) => usersController.find(request, h)
+    })
+
+    server.route({
+        method: 'POST',
+        path: '/users',
+        handler: (request, h) => usersController.create(request, h),
+        options: {
+            validate: {
+                payload: {
+                    firstName: Joi.string().required(),
+                    lastName: Joi.string().required(),
+                    email: Joi.string().email().required(),
+                }
+            }
+        }
+    })
+
+    server.route({
+        method: 'PUT',
+        path: '/users/{id}',
+        handler: (request, h) => usersController.update(request, h),
+        options: {
+            validate: {
+                payload: {
+                    firstName: Joi.string(),
+                    lastName: Joi.string(),
+                    email: Joi.string().email()
+                }
+            }
+        }
+    })
+
+    server.route({
+        method: 'DELETE',
+        path: '/users/{id}',
+        handler: (request, h) => usersController.delete(request, h)
+    })
+}
+
+module.exports = userRoute
